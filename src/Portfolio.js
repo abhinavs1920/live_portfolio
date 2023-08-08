315c05d4c32fc56e254d75a67f4fb8d07b4f71d7
= <=>
2023-08-08 19:45:24 +0530

    Hoja Bhai

---
+++ src/Portfolio.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <>
+      <nav className="bg-gray-800 text-white py-4">
+      </nav>
+
+      <header id="home" className="bg-indigo-500 text-white py-10">
+      </header>
+
+      <section id="skills" className="bg-gray-100 py-16">
+      </section>
+
+      <section id="projects" className="bg-gray-200 py-16">
+      </section>
+
+      <section id="blog" className="bg-gray-300 py-16">
+      </section>
+
+      <section id="about" className="bg-gray-400 py-16">
+      </section>
+
+      <section id="contact" className="bg-gray-500 py-16">
+      </section>
+
+      <footer className="bg-gray-800 text-white py-6">
+      </footer>
+    </>
+  );
+};
+
+export default Portfolio;
